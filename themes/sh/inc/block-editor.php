ac463a4d15b08f1a9d634299e5bbde31ac6fd90a
harshil kakdiya <Harshilkakdiya9@gmail.com>
2022-02-17 20:19:38 -0500

    installing and setting up woocommerce

---
+++ themes/sh/inc/block-editor.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Functions which enhance the theme by hooking into WordPress
+ *
+ * @package Sneaker_Head
+ */
+
+
+function sh_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'block-editor-script', 
+       get_template_directory_uri() . '/assets/js/block-editor.js',
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( get_template_directory() . '/assets/js/block-editor.js' )
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'sh_enqueue_block_editor_assets' );
+
+function sh_enqueue_block_assets() {
+    wp_enqueue_style(
+         'block-editor-style', 
+         get_template_directory_uri() . '/assets/css/block-editor.css'
+        );
+}
+add_action( 'enqueue_block_assets', 'sh_enqueue_block_assets' );
