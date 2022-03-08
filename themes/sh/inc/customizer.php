6dc43645049bb1e269c1e822959ebe3384e0be44
harshil kakdiya <Harshilkakdiya9@gmail.com>
2022-03-08 00:50:04 -0500

    social media  link customizer added

--- themes/sh/inc/customizer.php
+++ themes/sh/inc/customizer.php
@@ -31,6 +31,28 @@ function sh_customize_register( $wp_customize ) {
 			)
 		);
 	}
+
+	$wp_customize->add_section('sh_social_media', array(
+	'title' => esc_html__( 'Social media', 'sh' ),	
+	));
+
+	$wp_customize->add_setting(	'sh_facebook_url' );
+
+	$wp_customize->add_control(	'sh_facebook_url', array(
+		'label' 		=>	'Fcebook Url',
+		'description' 	=> 'Enter Your Facebook Profile Link ',
+		'type'  		=> 	'url',
+		'section'  		=> 	'sh_social_media',
+	 ) );
+
+	 $wp_customize->add_setting(	'sh_twitter_url' );
+
+	$wp_customize->add_control(	'sh_twitter_url', array(
+		'label' 		=>	'twitter Url',
+		'description' 	=> 'Enter Your twitter Profile Link ',
+		'type'  		=> 	'url',
+		'section'  		=> 	'sh_social_media',
+	 ) );
 }
 add_action( 'customize_register', 'sh_customize_register' );
 
