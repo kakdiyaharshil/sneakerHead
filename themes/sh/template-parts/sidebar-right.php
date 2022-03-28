24f7ac09efc0c1cbe1228fea946eb967169d1d55
harshil kakdiya <Harshilkakdiya9@gmail.com>
2022-03-28 14:08:52 -0500

    added foundation style to sidebar-right.php

--- themes/sh/template-parts/sidebar-right.php
+++ themes/sh/template-parts/sidebar-right.php
@@ -15,23 +15,37 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<div class="grid-container">
+			<div class="grid-x grid-margin-x grid-margin-y">
 
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
+			?>
+		<div class=" cell small-12 medium-8"> 
+			<?php
 			get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+			?>
+		</div>
+		<div class="cell small-12 medium-4">
+			<?php get_sidebar(); ?>
+		</div>
 
+		<?php
 		endwhile; // End of the loop.
 		?>
+
+		
+		</div>
+	</div>
+
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
