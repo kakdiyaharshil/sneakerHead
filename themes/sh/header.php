c759330b1428708643d7b4ee50cff10503500ea0
harshil kakdiya <Harshilkakdiya9@gmail.com>
2022-03-29 12:30:02 -0500

    created responsive menu

--- themes/sh/header.php
+++ themes/sh/header.php
@@ -49,9 +49,9 @@
 			}
 			?>
 		</div><!-- .site-branding -->
-
+			
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'sh' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="true"><?php esc_html_e( 'Primary Menu', 'sh' ); ?></button>
 			<?php
 			if(has_nav_menu( 'menu-primary' )) {
 				wp_nav_menu(
